a5ce06cbbda9aa0bda504ca09c6acb6a11ae9a27
Russell Gordon <rgordon@lcs.on.ca>
2018-08-15 17:46:24 -0400

    Initial Commit

---
+++ Blank Canvases.playground/Pages/Canvas 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Next page](@next) / [Previous page](@previous)
+//: # Canvas 1
+//: The following two statements are required to make the playground run. Please do not remove.
+import Cocoa
+import PlaygroundSupport
+
+// Create a new canvas
+let canvas = Canvas(width: 400, height: 600)
+
+// Begin working below...
+canvas.drawAxes()
+canvas.drawEllipse(centreX: 100 , centreY: 200, width: 50, height: 50)
+
+/*:
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+ */
+PlaygroundPage.current.liveView = canvas.imageView
---
+++ Blank Canvases.playground/Pages/Details.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+/*:
+ [Next page](@next)
+ # Canvas
+ 
+ Mac OS X provides an API for basic grahpics, but it is, I believe, too challenging for beginning programmers to work with.
+ 
+ I developed an abstraction layer, the Canvas class, that, together with auto-complete in Xcode, provides a discoverable, easy-to-use approach to programming with graphics.
+ 
+ The following five playground pages provide "blank canvases" that you can use to begin working with graphics in Swift.
+ 
+ [Next page](@next)
+ 
+ */
